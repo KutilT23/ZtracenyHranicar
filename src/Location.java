bede006e4051a8497d569b34ac7e751d5a28acba
KutilT23 <kutil@spsejecna.cz>
2025-02-22 14:03:39 +0100

    Přidána metoda na přečtení z textového souboru a textový soubor

---
+++ src/Location.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Location {
+
+    private String name;
+    private int ID;
+    private int[] locations;
+
+    public Location(){
+
+    }
+
+    public Location(String name, int ID, String[] locations) {
+        this.name = name;
+        this.ID = ID;
+        this.locations = new int[4];
+        for (int i = 0; i < locations.length; i++) {
+            this.locations[i] = Integer.parseInt(locations[i]);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "name='" + name + '\'' +
+                ", ID=" + ID +
+                ", locations=" + Arrays.toString(locations) +
+                '}';
+    }
+}
