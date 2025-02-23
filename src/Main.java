5cb27d58e187de04a9ef125dce6a553f1975c384
KutilT23 <kutil@spsejecna.cz>
2025-02-23 12:45:36 +0100

    Testování metod

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        WorldMap m = new WorldMap();
+        m.loadMap();
+        System.out.println(m.getWorld());
+        System.out.println(m.getCurrentPosition());
+        System.out.println(m.move("Sever"));
+        System.out.println(m.move("Jih"));
+        System.out.println(m.move("Jih"));
+        System.out.println(m.getCurrentPosition());
+        System.out.println(m.move("sever"));
+        System.out.println(m.move("vychod"));
+        System.out.println(m.move("vychod"));
+        System.out.println(m.move("vychod"));
+        System.out.println(m.move("vychod"));
+        System.out.println(m.move("zapad"));
+        System.out.println(m.move("jih"));
+
     }
 }
