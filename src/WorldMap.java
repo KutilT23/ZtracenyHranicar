0be03dfa36f1ba578e3187b606990211981a452f
KutilT23 <kutil@spsejecna.cz>
2025-02-23 12:46:11 +0100

    Přidání metody na pohyb po mapě

--- src/WorldMap.java
+++ src/WorldMap.java
@@ -17,7 +17,7 @@ public class WorldMap {
                 Location location = new Location(
                         lines[1],
                         Integer.parseInt(lines[0]),
-                        Arrays.copyOfRange(lines, 2, 5)
+                        Arrays.copyOfRange(lines, 2, 6)
                 );
                 world.put(Integer.valueOf(lines[0]), location);
             }
@@ -34,5 +34,32 @@ public class WorldMap {
 
     public HashMap<Integer, Location> getWorld() {
         return world;
+    }
+    public String move(String direction) {
+        int dirIndex;
+        switch (direction.toLowerCase()) {
+            case "sever":
+                dirIndex = 0;
+                break;
+            case "jih":
+                dirIndex = 1;
+                break;
+            case "vychod":
+                dirIndex = 2;
+                break;
+            case "zapad":
+                dirIndex = 3;
+                break;
+            default:
+                return "Neplatný směr! Použijte Sever, Jih, Východ nebo Západ.";
+        }
+
+        int newPosition = world.get(currentPosition).getLocations()[dirIndex];
+        if (newPosition == -1) {
+            return "Tímto směrem nelze jít.";
+        } else {
+            currentPosition = newPosition;
+            return "Přesunuli jste se na " + world.get(currentPosition).getName();
+        }
     }
 }
